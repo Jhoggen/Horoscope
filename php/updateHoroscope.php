533366ab2f34617132c8cca3f40330b9d32d8d6f
Jacob Hoggen <jacob.hoggen@medieinstitutet.se>
2021-12-19 16:23:40 +0100

    load

---
+++ php/updateHoroscope.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+    if(isset($_SERVER["REQUEST_METHOD"])) {
+
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            if(isset($_POST["date"])) {
+                if(isset($_SESSION["hscope"])) {
+
+                    require("listHoroscope.php");
+
+                        $_SESSION["hscope"] = serialize(fetchHscope($_POST["date"]));
+                        echo json_encode(unserialize($_SESSION["hscope"]));
+                }
+
+            } else {
+                
+                echo json_encode("Datum ej inmatat");
+            }
+        } else {
+            echo json_encode("Ingen POST metod");
+        }
+    } else {
+        echo json_encode("Ingen giltig förfrågan");
+    }
+
+?>
---
+++ script.js
@@ -0,0 +1,108 @@
+    
+    
+    function viewHoroscope() {
+        fetch("./php/viewHoroscope.php", {
+            method: "GET",
+        
+        }).then((data) => {
+            return data.json()
+
+        }).then((result) => {
+            console.log(result)
+            loadHoroscope(result)
+        }).catch((err) => {
+            console.log(err)
+        })
+      }
+      function loadHoroscope(hscope) {
+        let div = document.getElementById("loadHoroscope")
+
+        if(hscope) {
+            div.innerText = hscope
+        } else {
+            div.innerText = ""
+        }
+      }
+    
+    
+    function addHoroscope() {
+
+        document.getElementById(date)
+
+            let datum = date.value;
+            console.log(datum)
+
+            let url = "./php/addHoroscope.php"
+            let method = "POST"
+            let form = new FormData()
+            form.set("date", datum)
+
+                fetch(url, {
+                    method: method,
+                    body: form
+
+                }).then((response) => {
+                    console.log(response)
+                    return response.json()
+
+                }).then((result) => {
+                    console.log(result)
+                    viewHoroscope()
+                    loadHoroscope(result)
+
+                }).catch((err) => {
+                    console.log("Error: ", err)
+                })
+                
+    }
+
+
+      function updateHoroscope() {
+
+        document.getElementById(date)
+
+            let datum = date.value;
+            console.log(datum + "updaterat!")
+
+            let url = "./php/updateHoroscope.php"
+            let method = "POST"
+            let form = new FormData()
+            form.set("date", datum)
+
+                fetch(url, {
+                    method: method,
+                    body: form
+
+                }).then((response) => {
+                    console.log(response)
+                    return response.json()
+
+                }).then((result) => {
+                    console.log(result)
+                    viewHoroscope()
+
+                }).catch((err) => {
+                    console.log("Error: ", err)
+                })
+                
+      }
+
+
+      function deleteHoroscope() {
+
+            fetch("./php/deleteHoroscope.php", {
+                method: "DELETE",
+
+            }).then((data) => {
+                return data.json()
+
+            }).then((result) => {
+                console.log(result)
+                viewHoroscope()
+                loadHoroscope(result)
+
+            }).catch((err) => {
+                console.log(err)
+            })
+            console.log("Sparat horoskop borttaget")
+      }
